c575b3f36c3026f10db8b84c1238635f752e107d
Alex Curley <acurley1431@gmail.com>
2023-09-07 17:24:54 -0400

    Added load balancer to production ALLOWED_HOSTS

--- app/settings/production.py
+++ app/settings/production.py
@@ -3,4 +3,6 @@ from app.settings.base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = []  # FIXME: configure
+ALLOWED_HOSTS = [
+    "pss-production-1462576609.us-east-1.elb.amazonaws.com", # FIXME: replace with production domains
+]
