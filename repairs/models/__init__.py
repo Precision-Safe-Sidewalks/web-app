b47afb178d21cc930d0362484cae427a12294aab
Alex Curley <acurley1431@gmail.com>
2023-09-11 09:39:25 -0400

    Added Instruction, InstructionSpecification, InstructionNote models.

--- repairs/models/__init__.py
+++ repairs/models/__init__.py
@@ -1,3 +1,8 @@
 # ruff: noqa
+from repairs.models.instructions import (
+    Instruction,
+    InstructionSpecification,
+    InstructionNote,
+)
 from repairs.models.measurements import Measurement, MeasurementImage
 from repairs.models.projects import Project, ProjectContact
---
+++ repairs/models/instructions.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+from repairs.models.constants import Stage
+from repairs.models.projects import Project
+
+
+class Instruction(models.Model):
+    """Survey/Project instructions set"""
+
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name="instructions"
+    )
+    stage = models.CharField(max_length=25, choices=Stage.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # TODO: survey method (SI only)
+    # TODO: required number of images + sizes
+    # TODO: cuts (PI only)
+
+
+class InstructionSpecification(models.Model):
+    """Survey/Project instructions specifications set"""
+
+    class SpecificationType(models.TextChoices):
+        """Specification type choices"""
+
+        HAZARD = ("H", "Hazard")
+        SPECIAL_CASE = ("SC", "Special case")
+        DR = ("DR", "D&R specification")
+
+    instruction = models.ForeignKey(
+        Instruction, on_delete=models.CASCADE, related_name="specifications"
+    )
+    specification_type = models.CharField(
+        max_length=10, choices=SpecificationType.choices
+    )
+    specification = models.CharField(max_length=10)
+    area = models.FloatField(blank=True, null=True)
+    note = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ("instruction", "specification_type", "specification")
+
+
+class InstructionNote(models.Model):
+    """Survey/Project instructions note set"""
+
+    instruction = models.ForeignKey(
+        Instruction, on_delete=models.CASCADE, related_name="notes"
+    )
+    note = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
