45c41795b9892a7e12e68684ccc94aa9daa3a51d
Alex Curley <acurley1431@gmail.com>
2023-09-12 14:48:45 -0400

    Updated contact form

--- pages/forms/contacts.py
+++ pages/forms/contacts.py
@@ -5,6 +5,16 @@ from customers.models import Contact
 
 
 class ContactForm(forms.ModelForm):
+    phone_work = models.CharField(max_length=25, required=False)
+    phone_work_ext = models.CharField(max_length=10, required=False)
+    phone_cell = models.CharField(max_length=25, required=False)
+    phone_cell_ext = models.CharField(max_length=10, required=False)
+
+    def clean_phone_work(self):
+        raise NotImplementedError
+
+    def clean_phone_cell(self):
+        raise NotImplementedError
 
     def clean_phone_number(self):
         phone_number = self.cleaned_data["phone_number"]
