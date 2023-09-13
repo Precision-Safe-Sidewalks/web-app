1935b02ef1ce76fa20fc02618212fb172b55e728
Alex Curley <acurley1431@gmail.com>
2023-09-13 08:13:23 -0400

    Added multi select fragment

---
+++ pages/templatetags/core_tags.py
@@ -0,0 +1,10 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def isin(value, options):
+    """Return True if the value is in the options"""
+    return value in options
