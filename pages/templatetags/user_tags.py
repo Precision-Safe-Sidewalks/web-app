602e52f969ef6ada25a23b4d1dbf6afe685e6ac5
Alex Curley <acurley1431@gmail.com>
2023-09-13 15:44:40 -0400

    Fix user form. Fix user tags.

--- pages/templatetags/user_tags.py
+++ pages/templatetags/user_tags.py
@@ -6,5 +6,7 @@ register = template.Library()
 @register.simple_tag
 def has_role(user, role):
     """Return True if the user has the role"""
-    print(user, role, user.roles.all().values("role"))
+    if not user:
+        return False
+
     return user.roles.filter(role=role).exists()
