9c502bd46fabbe5273d08c723ab285c4b60483ed
Alex Curley <acurley1431@gmail.com>
2024-01-05 07:48:36 -0500

    Fix testing factories

--- repairs/factories.py
+++ repairs/factories.py
@@ -1,8 +1,6 @@
 import factory
 import factory.fuzzy
 
-from accounts.factories import UserFactory
-from core.factories import TerritoryFactory
 from customers.factories import CustomerFactory
 from repairs.models import Project
 
@@ -10,8 +8,6 @@ from repairs.models import Project
 class ProjectFactory(factory.django.DjangoModelFactory):
     name = factory.fuzzy.FuzzyText(length=10)
     customer = factory.SubFactory(CustomerFactory)
-    territory = factory.SubFactory(TerritoryFactory)
-    business_development_manager = factory.SubFactory(UserFactory)
 
     class Meta:
         model = Project
