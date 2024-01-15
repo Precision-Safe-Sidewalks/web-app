b37df49ec291d328ba386d33e129aee56a709424
Alex Curley <acurley1431@gmail.com>
2024-01-15 08:14:43 -0500

    Add autocomplete to ArcGIS item api view

--- api/filters/arcgis.py
+++ api/filters/arcgis.py
@@ -1,10 +1,17 @@
 import django_filters
+from django.db.models import Q
 
 from third_party.models import ArcGISItem
 
 
 class ArcGISItemFilter(django_filters.FilterSet):
     """ArcGIS Item filter set"""
+
+    autocomplete = django_filters.CharFilter(method="filter_autocomplete")
+
+    def filter_autocomplete(self, queryset, name, value):
+        """Filter by the autocomplete query"""
+        return queryset.filter(title__istartswith=value)
 
     class Meta:
         model = ArcGISItem
