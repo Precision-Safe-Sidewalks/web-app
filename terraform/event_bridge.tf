a4a497f424ca93e5ae8898c8adcdbb314553f797
Alex Curley <acurley1431@gmail.com>
2024-01-15 19:43:21 -0500

    Execute ArcGIS sync every 4 hours

--- terraform/event_bridge.tf
+++ terraform/event_bridge.tf
@@ -1,12 +1,12 @@
-resource "aws_cloudwatch_event_rule" "hourly" {
-  name                = "hourly"
-  description         = "Every hour periodic event"
-  schedule_expression = "rate(1 hour)"
+resource "aws_cloudwatch_event_rule" "periodic" {
+  name                = "periodic"
+  description         = "Periodic task event rule (every 4 hours)"
+  schedule_expression = "rate(4 hour)"
 }
 
 resource "aws_cloudwatch_event_target" "arcgis_sync" {
   arn       = aws_lambda_function.arcgis_sync.arn
-  rule      = aws_cloudwatch_event_rule.hourly.name
+  rule      = aws_cloudwatch_event_rule.periodic.name
   target_id = "arcgis_sync"
 }
 
@@ -15,5 +15,5 @@ resource "aws_lambda_permission" "arcgis_sync" {
   action        = "lambda:InvokeAction"
   function_name = aws_lambda_function.arcgis_sync.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.hourly.arn
+  source_arn    = aws_cloudwatch_event_rule.periodic.arn
 }
